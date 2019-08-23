a9287b3f3f10baf2e60533a66d7377dd83e5f74e
misi <18435@toba.kosen-ac.jp>
2019-08-23 14:36:40 +0900

    再生機能の追加

---
+++ Assets/Speed.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Speed: MonoBehaviour
+{
+    GvrControllerInputDevice gvrControllerInput;
+
+    void Start()
+    {
+        gvrControllerInput = GvrControllerInput.GetDevice(GvrControllerHand.Dominant);
+    }
+
+    [SerializeField]
+    //　ポーズした時に表示するUIのプレハブ
+    private GameObject pauseUIPrefab;
+    //　ポーズUIのインスタンス
+    private GameObject pauseUIInstance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gvrControllerInput.GetButtonDown(GvrControllerButton.System))
+        {
+            if (pauseUIInstance == null)
+            {
+                pauseUIInstance = GameObject.Instantiate(pauseUIPrefab) as GameObject;
+                Time.timeScale = 0.5f;
+            }
+            else
+            {
+                Destroy(pauseUIInstance);
+                Time.timeScale = 1f;
+            }
+        }
+    }
+}
